442ed86f645c0e2ae06159db5fa314722505402e
Gilles Henrard <gilles.henrard@hotmail.com>
2020-04-19 00:09:34 +0200

    fix timer1 configuration to make it 10 Hz

--- DataLogger.ino
+++ DataLogger.ino
@@ -45,13 +45,13 @@ void setup() {
   TCNT1  = 0;
   
   // set compare match register for 10 Hz increments
-  OCR1A = 12499; // = 8000000 / (256 * 1) - 1 (must be <65536)
+  OCR1A = 12499; // = 8000000 / (64 * 10) - 1 (must be <65536)
   
   // turn on CTC mode
   TCCR1B |= (1 << WGM12);
   
-  // Set CS12, CS11 and CS10 bits for 256 prescaler
-  TCCR1B |= (1 << CS12) | (0 << CS11) | (0 << CS10);
+  // Set CS12, CS11 and CS10 bits for 64 prescaler
+  TCCR1B |= (0 << CS12) | (1 << CS11) | (1 << CS10);
   
   // enable timer compare interrupt
   TIMSK1 |= (1 << OCIE1A);
